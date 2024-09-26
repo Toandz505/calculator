37a0d3ac9f01c2867af963b5e6643ac3b2cf3db5
Toandz505 <quoctoan01255@gmail.com>
2024-09-26 13:45:13 +0700

    Them xy ly Button

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,13 @@ namespace Calculator
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e) {
+            MessageBox.Show("asf");
+            MessageBox.Show("Thanh vien 2");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanh vien 2");
+        }
     }
 }
